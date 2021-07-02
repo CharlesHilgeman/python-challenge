5ce0e77260e27bdd0a5ba9d1f6ef256d612d802e
CharlesHilgeman <cchilgeman@gatech.edu>
2021-07-02 13:15:49 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join("Resorces", "03-Python_02-Homework_Instructions_PyBank_Resources_budget_data.csv")
+csvpath = os.path.join("Resources","budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile,delimiter = ",")
     print(csvreader)
-
-csvheader = next(csvreader)
-print(csvheader)
