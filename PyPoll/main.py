df7130234c515c2dc6618c67c3c4c903c0eeff0b
CharlesHilgeman <cchilgeman@gatech.edu>
2021-07-05 14:45:17 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,3 +5,12 @@ csvpath = os.path.join("02-Homework/python-challenge/PyPoll/Resources/election_d
 with open(csvpath) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",")
     csvreader = next(csv_reader, None)
+
+ids = []
+county = []
+candidate = []
+
+for row in csvreader:
+    ids.append(row[0])
+    county.append(row[1])
+    candidate.append(row[2])
