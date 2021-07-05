2e54181ba6f475092a0b3ac3825841b4d4f59502
CharlesHilgeman <cchilgeman@gatech.edu>
2021-07-05 19:40:38 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,8 +18,41 @@ with open(csvpath) as csvfile:
     voters = 0
     for id in ident:
         voters += 1
+    
+    khanvote = 0
+    correyvote = 0
+    livote = 0
+    otoolvote = 0
 
+    
+    for num in candidate:
+        if num == "Khan":
+            khanvote += 1
+        elif num == "Correy":
+            correyvote += 1
+        elif num == "Li":
+            livote += 1
+        else:
+            otoolvote += 1
+    
+    khanperc = (khanvote/voters) *100
+    formatkhan = "{:.3f}".format(khanperc)
+    correyperc = (correyvote/voters) *100
+    formatcorrey = "{:.3f}".format(correyperc)
+    liperc = (livote/voters) *100
+    formatli = "{:.3f}".format(liperc)
+    otoolperc = (otoolvote/voters) *100
+    formatotool = "{:.3f}".format(otoolperc)
 
-print(str(voters))
+        
+
+print("Election Results")
+print("--------------------")
+print("Total Votes: " + str(voters))
+print("--------------------")
+print("Khan: " + str(formatkhan) + "% (" + str(khanvote)+ ")" )
+print("Correy: " + str(formatcorrey) + "% (" + str(correyvote)+ ")" )
+print("Li: " + str(formatli) + "% (" + str(livote) + ")" )
+print("O'Tooley: " + str(formatotool) + "% (" + str(otoolvote)+ ")" )
 
 
